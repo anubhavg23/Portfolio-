d3793470cb59418c600a6b1c6c5e30c694e16afb
Anubhav Gupta <anubhavg430@gmail.com>
2025-03-21 20:07:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Full-Stack Developer", "Pythoniasts","Web Developer","Java Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+})
+// let popup =document.getElementById("popup");
+// function openPopup(){
+//     popup.classList.add("open-popup");
+// }
+// function closePopup(){
+//     popup.classList.remove("open-popup");
+// }
